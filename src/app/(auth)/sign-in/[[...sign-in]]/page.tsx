9714c3cf166401617f3ddfe77ff7b33859bf4aaa
Parna Roy Chowdhury <123104247+ParnaRoyChowdhury777@users.noreply.github.com>
2025-03-02 02:58:42 +0530

    sign in

--- src/app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ src/app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -1,132 +1,174 @@
 "use client";
 
-import React, { useState } from "react";
-import { useSignIn } from "@clerk/nextjs";
-import { useRouter } from "next/navigation";
+import * as Clerk from "@clerk/elements/common";
+import * as SignIn from "@clerk/elements/sign-in";
 import { Button } from "@/components/ui/button";
-import { Label } from "@/components/ui/label";
-import { toast } from "react-toastify";
-import Link from "next/link";
-import { LoaderIcon, Mail, Lock } from "lucide-react";
+import { Mail, Loader, Lock } from "lucide-react";
 
-function SignIn() {
-  const { isLoaded, signIn, setActive } = useSignIn();
-
-  const router = useRouter();
-
-  const [email, setEmail] = useState<string>("");
-  const [password, setPassword] = useState<string>("");
-  const [isLoading, setIsLoading] = useState<boolean>(false);
-
-  const handleSubmit = async (e: React.FormEvent) => {
-    e.preventDefault();
-    if (!isLoaded) return;
-
-    if (!email || !password) {
-      return toast.warning("Please fill out all fields.");
-    }
-
-    setIsLoading(true);
-
-    try {
-      const signInAttempt = await signIn.create({
-        identifier: email,
-        password,
-        redirectUrl: "/auth-callback",
-      });
-
-      if (signInAttempt.status === "complete") {
-        await setActive({ session: signInAttempt.createdSessionId });
-        router.push("/auth-callback");
-      } else {
-        console.error(JSON.stringify(signInAttempt, null, 2));
-        toast.error("Invalid email or password. Please try again.");
-      }
-      // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    } catch (error: any) {
-      switch (error.errors[0]?.code) {
-        case "form_identifier_not_found":
-          toast.error("This email is not registered. Please sign up first.");
-          break;
-        case "form_password_incorrect":
-          toast.error("Incorrect password. Please try again.");
-          break;
-        case "too_many_attempts":
-          toast.error(
-            "Too many attempts. Please try again later or reset your password."
-          );
-          break;
-        default:
-          toast.error("An error occurred. Please try again.");
-      }
-    } finally {
-      setIsLoading(false);
-    }
-  };
-
+export default function SignInPage() {
   return (
     <div className="flex flex-col items-center justify-center hc gap-y-6">
-      <h1 className="text-2xl text-center font-bold">Sign In</h1>
-      <form
-        onSubmit={handleSubmit}
-        className="w-full max-w-sm space-y-4 border p-7 rounded-lg shadow-lg"
-      >
-        <div className="space-y-2">
-          <Label htmlFor="email">Email Address</Label>
-          <div className="flex items-center w-full px-3 py-2 border rounded-lg shadow-sm focus-within:border-blue-600 bg-white">
-            <Mail className="w-5 h-5 text-muted-foreground" />
-            <input
-              id="email"
-              type="email"
-              name="email"
-              value={email}
-              disabled={isLoading}
-              onChange={(e) => setEmail(e.target.value)}
-              placeholder="Enter your email address"
-              className="w-full ml-2 bg-transparent outline-none text-sm placeholder-gray-500 p-0.5"
-            />
-          </div>
-        </div>
-        <div className="space-y-2">
-          <Label htmlFor="password">Password</Label>
-          <div className="flex items-center w-full px-3 py-2 border rounded-lg shadow-sm focus-within:border-blue-600 bg-white">
-            <Lock className="w-5 h-5 text-muted-foreground" />
-            <input
-              id="password"
-              type="password"
-              name="password"
-              value={password}
-              disabled={isLoading}
-              onChange={(e) => setPassword(e.target.value)}
-              placeholder="Enter your password"
-              className="w-full ml-2 bg-transparent outline-none text-sm placeholder-gray-500 p-0.5"
-            />
-          </div>
-        </div>
-        <div id="clerk-captcha"></div>
-        <Button
-          size="lg"
-          type="submit"
-          disabled={isLoading}
-          className="w-full bg-blue-500 hover:bg-blue-600"
+      <SignIn.Root>
+        <SignIn.Step
+          name="start"
+          className="w-full max-w-sm space-y-4 border p-7 rounded-lg shadow-lg"
         >
-          {isLoading ? (
-            <LoaderIcon className="animate-spin w-5 h-5" />
-          ) : (
-            "Sign In"
-          )}
-        </Button>
-        <div className="flex">
+          <h1 className="text-2xl text-center font-bold">
+            Sign in to your account
+          </h1>
+
+          <Clerk.Connection name="google" className="w-full">
+            <Clerk.Loading scope="provider:google">
+              {(isLoading) =>
+                isLoading ? (
+                  <div className="w-full flex items-center justify-center gap-x-3 py-2.5 border rounded-lg text-sm font-medium duration-150">
+                    <Loader className="size-4 animate-spin" />
+                  </div>
+                ) : (
+                  <div className="w-full flex items-center justify-center gap-x-3 py-2.5 border rounded-lg text-sm font-medium hover:bg-gray-50 duration-150 active:bg-gray-100">
+                    <Clerk.Icon className="size-4" />
+                    Continue with Google
+                  </div>
+                )
+              }
+            </Clerk.Loading>
+          </Clerk.Connection>
+
+          <div className="flex items-center w-full gap-3">
+            <div className="flex-grow border-t border-gray-400"></div>
+            <span className="text-gray-600">or</span>
+            <div className="flex-grow border-t border-gray-400"></div>
+          </div>
+
+          <Clerk.Field name="identifier" className="flex flex-col space-y-2">
+            <Clerk.Label className="text-sm">Email</Clerk.Label>
+            <div className="flex items-center w-full px-3 py-2 border rounded-lg shadow-sm focus-within:border-blue-600 bg-white">
+              <Mail className="w-5 h-5 text-muted-foreground" />
+              <Clerk.Input
+                type="email"
+                placeholder="Enter your email address"
+                className="w-full ml-2 bg-transparent outline-none text-sm placeholder-gray-500 p-0.5"
+                required
+              />
+            </div>
+            <Clerk.FieldError className="block text-red-500 text-sm" />
+          </Clerk.Field>
+
+          <SignIn.Action submit asChild>
+            <Button className="w-full">
+              <Clerk.Loading>
+                {(isLoading) =>
+                  isLoading ? (
+                    <Loader className="size-4 animate-spin" />
+                  ) : (
+                    "Sign In"
+                  )
+                }
+              </Clerk.Loading>
+            </Button>
+          </SignIn.Action>
           <p className="text-sm text-muted-foreground text-center w-full">
             Don&apos;t have an account?{" "}
-            <Link href="sign-up" className="text-blue-500 hover:underline">
+            <Clerk.Link
+              navigate="sign-up"
+              className="text-blue-500 hover:underline"
+            >
               Sign up
-            </Link>
+            </Clerk.Link>
           </p>
-        </div>
-      </form>
+        </SignIn.Step>
+
+        <SignIn.Step
+          name="verifications"
+          className="w-full max-w-sm space-y-4 border p-7 rounded-lg shadow-lg"
+        >
+          <SignIn.Strategy name="password">
+            <h1 className="text-2xl text-center font-bold">
+              Enter your password
+            </h1>
+
+            <Clerk.Field name="password" className="flex flex-col space-y-2">
+              <Clerk.Label className="text-sm">Password</Clerk.Label>
+              <div className="flex items-center w-full px-3 py-2 border rounded-lg shadow-sm focus-within:border-blue-600 bg-white">
+                <Lock className="w-5 h-5 text-muted-foreground" />
+                <Clerk.Input
+                  type="password"
+                  validatePassword
+                  placeholder="Enter your password"
+                  className="w-full ml-2 bg-transparent outline-none text-sm placeholder-gray-500 p-0.5"
+                  required
+                />
+              </div>
+              <Clerk.FieldError className="block text-red-500 text-sm" />
+            </Clerk.Field>
+
+            <SignIn.Action submit asChild>
+              <Button className="w-full mb-0">
+                <Clerk.Loading>
+                  {(isLoading) =>
+                    isLoading ? (
+                      <Loader className="size-4 animate-spin" />
+                    ) : (
+                      "Continue"
+                    )
+                  }
+                </Clerk.Loading>
+              </Button>
+            </SignIn.Action>
+            <SignIn.Action
+              navigate="forgot-password"
+              asChild
+              className="text-sm text-muted-foreground"
+            >
+              <div className="text-right ">
+                <Button variant="link" className="text-blue-500">
+                  Forgot password?
+                </Button>
+              </div>
+            </SignIn.Action>
+          </SignIn.Strategy>
+        </SignIn.Step>
+
+        <SignIn.Step
+          name="forgot-password"
+          className="w-full max-w-sm space-y-4 border p-7 rounded-lg shadow-lg"
+        >
+          <h1 className="text-2xl text-center font-bold">
+            Forgot your password?
+          </h1>
+
+          <div className="flex justify-between items-center">
+            <SignIn.SupportedStrategy name="reset_password_email_code" asChild>
+              <Button type="submit">Reset password</Button>
+            </SignIn.SupportedStrategy>
+
+            <SignIn.Action navigate="previous" asChild>
+              <Button>Go back</Button>
+            </SignIn.Action>
+          </div>
+        </SignIn.Step>
+
+        <SignIn.Step
+          name="reset-password"
+          className="w-full max-w-sm space-y-4 border p-7 rounded-lg shadow-lg"
+        >
+          <h1>Reset your password</h1>
+
+          <Clerk.Field name="password">
+            <Clerk.Label>New password</Clerk.Label>
+            <Clerk.Input />
+            <Clerk.FieldError />
+          </Clerk.Field>
+
+          <Clerk.Field name="confirmPassword">
+            <Clerk.Label>Confirm password</Clerk.Label>
+            <Clerk.Input />
+            <Clerk.FieldError />
+          </Clerk.Field>
+
+          <SignIn.Action submit>Reset password</SignIn.Action>
+        </SignIn.Step>
+      </SignIn.Root>
     </div>
   );
 }
-
-export default SignIn;
