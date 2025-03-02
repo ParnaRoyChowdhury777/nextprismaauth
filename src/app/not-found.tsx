daaa74765ddd9c1487dfaf3074a4b2eb986bd7a9
Parna Roy Chowdhury <123104247+ParnaRoyChowdhury777@users.noreply.github.com>
2025-03-02 16:55:15 +0530

    enhance NotFound page with a link to return home

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,14 +1,19 @@
 "use client";
 
 import React from "react";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 function NotFound() {
   return (
-    <div className="flex items-center justify-center hc w-full">
+    <div className="flex flex-col space-y-5 items-center justify-center hc w-full">
       <h4 className="text-center text-lg font-medium">
         It seems you&apos;ve hit a page that doesn&apos;t exist. Please check
         the URL and try again.
       </h4>
+      <Link href="/">
+        <Button>Go Back to Home</Button>
+      </Link>
     </div>
   );
 }
